3bfa9ec6795af6b29f1adb6ec02da616e192625f
Vivek-Mehra298 <vivekmehra298@gmail.com>
2026-06-11 18:52:47 +0530

    Fix module import error

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import router from './routes/user.js';
 
 const app=express();
 
@@ -7,6 +8,13 @@ const PORT=7000;
 app.get('/',(req,res)=>{
     res.send('Hey there! Welcome to the backend server.');
 });
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//Routes
+app.use(router);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,13 +1,19 @@
 import {Router} from 'express'
 
-import User from "../models/user";
+import User from "../models/user.js";
 
 const router=Router();
 
 router.post('/user',async(req,res)=>{
-    const {FullName,Email,Password}=req.body;
+    try{
+        const{FullName,Email,Password}=req.body;
+        await User.create({FullName,Email,Password});
+        res.status(201).json({message:"User created successfully"});
+    }
+    catch(error){
+        res.status(500).json({message:"Unknown error occured while creating user"});
+    }
 
-    await User.create({FullName,Email,Password});
 })
 
 export default router;
